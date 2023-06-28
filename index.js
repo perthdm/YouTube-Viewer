c3d10c5ce11d263e60e5e64c12f04ddc1adf9e80
perthdm <prathza55@hotmail.com>
2023-06-28 14:46:31 +0700

    etc

--- index.js
+++ index.js
@@ -12,46 +12,52 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-let conf = {
-  http_api: {
-    enabled: true,
-    host: "0.0.0.0",
-    port: 5000,
-  },
-  database: true,
-  views: 10,
-  minimum: 85.0,
-  maximum: 95.0,
-  proxy: {
-    category: "f",
-    proxy_type: "http",
-    filename: "GoodProxy.txt",
-    authentication: false,
-    proxy_api: false,
-    refresh: 0.0,
-  },
-  background: false,
-  bandwidth: true,
-  playback_speed: 1,
-  max_threads: 5,
-  min_threads: 2,
+let conf = (view_expect) => {
+  view_expect = view_expect ? view_expect : 1;
+  return {
+    http_api: {
+      enabled: true,
+      host: "0.0.0.0",
+      port: 5000,
+    },
+    database: true,
+    views: view_expect,
+    minimum: 85.0,
+    maximum: 95.0,
+    proxy: {
+      category: "f",
+      proxy_type: "http",
+      filename: "GoodProxy.txt",
+      authentication: false,
+      proxy_api: false,
+      refresh: 0.0,
+    },
+    background: false,
+    bandwidth: true,
+    playback_speed: 1,
+    max_threads: 5,
+    min_threads: 2,
+  };
 };
 
-app.get("/", (req, res) => {
-  // const proxyList = [
-  //   "171.6.73.223:3128",
-  //   "171.6.73.223:3129",
-  //   "171.6.73.223:3130",
-  // ];
+app.get("/check-proxy-available", (req, res) => {
+  // MOCK UP DATA
+  const proxyList = [
+    "171.6.73.223:3128",
+    "171.6.73.223:3129",
+    "171.6.73.223:3130",
+  ];
 
-  // let proxyTxt = "";
-  // if (proxyList.length > 0) {
-  //   proxyList.map((item) => {
-  //     proxyTxt += item + "\n";
-  //   });
-  // }
+  // Function get all proxy available
 
-  // writeFile("myproxy.txt", proxyTxt);
+  let proxyTxt = "";
+  if (proxyList.length > 0) {
+    proxyList.map((item) => {
+      proxyTxt += item + "\n";
+    });
+  }
+
+  writeFile("myproxy.txt", proxyTxt);
 
   const python = spawn("python3", ["proxy_check.py"], {
     stdio: "inherit",
@@ -64,12 +70,16 @@ app.get("/", (req, res) => {
   });
 });
 
-app.post("/", (req, res) => {
-  let { search_txt, video_url, video_id } = req.body;
+app.post("/confirm-order", (req, res) => {
+  let { search_txt, video_url, video_id, view_expect } = req.body;
 
   writeFile("urls.txt", video_url);
   writeFile("search.txt", `${search_txt} :::: ${video_id}`);
-  fs.writeFileSync("config.json", JSON.stringify(conf, null, 2), "utf-8");
+  fs.writeFileSync(
+    "config.json",
+    JSON.stringify(conf(view_expect), null, 2),
+    "utf-8"
+  );
 
   const python = spawn("python3", ["youtube_viewer.py"], {
     stdio: "inherit",
